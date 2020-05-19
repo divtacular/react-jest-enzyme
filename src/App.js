2bd457489e1466f86887daa283af5cdf1f375769
David Smythe <smythe.david@gmail.com>
2020-05-19 12:30:50 +0100

    Add initial state and test
    Counter initialised at 0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+    state = {
+        counter: 0
+    }
+
     render() {
         return (<div data-test={"component-app"}>
             <h1 data-test={"counter-display"}>The counter is currently: 0</h1>
