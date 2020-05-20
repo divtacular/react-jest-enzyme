7a3152b6bc1deb5497f49fd50bb2010052bd624a
David Smythe <smythe.david@gmail.com>
2020-05-20 14:45:16 +0100

    App tests
    Boilerplate added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Congrats from "./Congrats";
 class App extends Component {
     render() {
         return (
-            <div className={"container"}>
+            <div className={"container"} data-test={"component-app"}>
               <h1>Jotto</h1>
               <Congrats success={true} />
               <GuessedWords guessedWords={[
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import {shallow} from 'enzyme';
+// import {render} from "@testing-library/react";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import {findByTestAttr, checkProps} from "../test/testUtils";
+import App from './App';
+import Congrats from "./Congrats";
+
+const defaultProps = {};
+
+/**
+ * Factory function to create ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props - Component props for this setup
+ * @param {object} state - Initial state
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  const setupProps = { ...defaultProps, ...props};
+  const wrapper = shallow(<App {...setupProps} />);
+
+  if (state) {
+    wrapper.setState(state);
+  }
+  return wrapper;
+}
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-app');
+
+  expect(component.length).toBe(1);
 });
