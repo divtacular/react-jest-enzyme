0ca7bff006de87b6b414fee8460dc1120648adc2
David Smythe <smythe.david@gmail.com>
2020-05-20 11:55:40 +0100

    GuessedWords - Tests satisfied
    Wrote and satisfied test criteria for displaying guessed words.

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -6,6 +6,29 @@ const GuessedWords = (props) => {
 
     if (props.guessedWords.length === 0) {
         contents = <p data-test={"guess-instructions"}>Guess the secret word!</p>;
+    } else {
+
+        const guessedWordsRows = props.guessedWords.map(({guessedWord, letterMatchCount}, i) => {
+            return <tr key={i} data-test={"guessed-word"}>
+                <td>guessedWord</td>
+                <td>letterMatchCount</td>
+            </tr>
+        });
+
+        contents = <div data-test={"guessed-words"}>
+            <h3>Guessed Words</h3>
+            <table>
+                <thead>
+                <tr>
+                    <th>Guess</th>
+                    <th>Matching Letters</th>
+                </tr>
+                <tbody>
+                {guessedWordsRows}
+                </tbody>
+                </thead>
+            </table>
+        </div>;
     }
 
     return (
